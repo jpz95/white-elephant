8eedcd3804d130ba50a6fe25e14fc3aba6256a60
J.P. Zambrano <jpzambrano95@hotmail.com>
2021-01-23 15:41:17 -0600

    Gives responsive layout for player queue
     - For smaller screens, the player queue is reduced to a button, that opens
        a menu listing the players.
     - For bigger screens shows the player cards at the top (kinda like smash bros)

---
+++ src/components/dropdown-menu.vue
@@ -0,0 +1,26 @@
+<template>
+  <div
+    class="dropdownMenu position-absolute"
+    :class="{ show: open }"
+  >
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DropdownMenu',
+  props: {
+    open: { type: Boolean, default: false },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.dropdownMenu {
+  display: none;
+}
+.dropdownMenu.show {
+  display: block;
+}
+</style>
--- src/features/player-queue.vue
+++ src/features/player-queue.vue
@@ -1,18 +1,45 @@
 <template>
-  <ul class="playerQueue flex">
-    <li
-      v-for="player in playerQueue"
-      :key="player"
-      class="playerQueueCard flex justify-content-center align-items-center"
+  <div>
+    <ul class="playerQueue">
+      <li
+        v-for="player in playerQueue"
+        :key="player"
+        class="playerQueueCard flex justify-content-center align-items-center"
+      >
+        {{ player }}
+      </li>
+    </ul>
+    <button
+      class="playerQueueButton"
+      @click="showMenu = !showMenu"
     >
-      {{ player }}
-    </li>
-  </ul>
+      Player queue
+    </button>
+    <dropdown-menu
+      class="playerQueueDropdownMenu"
+      :open="showMenu"
+    >
+      <ul>
+        <li
+          v-for="player in playerQueue"
+          :key="player"
+          class="playerQueueMenuItem"
+        >
+          {{ player }}
+        </li>
+      </ul>
+    </dropdown-menu>
+  </div>
 </template>
 
 <script>
+import DropdownMenu from 'components/dropdown-menu.vue';
+
 export default {
   name: 'PlayerQueue',
+  components: {
+    DropdownMenu,
+  },
   props: {
     players: {
       type: Array,
@@ -23,6 +50,9 @@ export default {
       default: 0,
     },
   },
+  data: () => ({
+    showMenu: false,
+  }),
   computed: {
     playerQueue() {
       return this.players.slice(this.currentPlayerIdx + 1);
@@ -32,16 +62,61 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.playerQueue {
-  border-bottom-width: 2px;
-  border-right-width: 0;
-  background-image: linear-gradient(to top, #feada6 0%, #f5efef 100%);
-  border-bottom-right-radius: 0;
-  border-color: $color-secondary-main;
+@include screen-xs {
+  .playerQueue {
+    display: none;
+  }
+}
+@include screen-md {
+  .playerQueue {
+    display: flex;
+    border-bottom-width: 2px;
+    border-right-width: 0;
+    background-image: linear-gradient(to top, #feada6 0%, #f5efef 100%);
+    border-bottom-right-radius: 0;
+    border-color: $color-secondary-main;
+  }
 }
 
 .playerQueueCard {
   height: 6.5em;
   width: 9em;
+}
+
+@include screen-xs {
+  .playerQueueButton {
+    display: flex;
+  }
+}
+@include screen-md {
+  .playerQueueButton {
+    display: none;
+  }
+}
+
+.playerQueueDropdownMenu {
+  height: 100%;
+  width: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 1;
+}
+@include screen-sm {
+  .playerQueueDropdownMenu {
+    height: 85%;
+    width: 75%;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+}
+@include screen-md {
+  .playerQueueDropdownMenu {
+    display: none;
+  }
+  .playerQueueDropdownMenu.show {
+    display: none;
+  }
 }
 </style>
